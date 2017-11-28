12f13fc5f33b2f29d369e1dd8e5b66adcc05764b
nazomeku <cols.nazo@gmail.com>
2017-11-28 19:19:23 +0100

    initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est aspernatur quo odit sint explicabo repellendus, neque libero laudantium nobis sequi rerum accusamus perferendis sunt beatae tenetur suscipit corrupti non.',
+            'author':'Jane Doe',
+            'create date':'28-11-2017'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est aspernatur quo odit sint explicabo repellendus, neque libero laudantium nobis sequi rerum accusamus perferendis sunt beatae tenetur suscipit corrupti non.',
+            'author':'John Doe',
+            'create date':'28-11-2017'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est aspernatur quo odit sint explicabo repellendus, neque libero laudantium nobis sequi rerum accusamus perferendis sunt beatae tenetur suscipit corrupti non.',
+            'author':'Jimmy Doe',
+            'create date':'28-11-2017'
+        }
+    ]
+    return articles
