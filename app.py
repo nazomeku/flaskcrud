6d2bab0b8a304832a0618ae0730f8b8ee3bf5ea4
nazomeku <cols.nazo@gmail.com>
2017-12-01 21:14:02 +0100

    add articles

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from functools import wraps
+from flask import Flask, render_template, flash, redirect, url_for, session, request
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 from passlib.hash import sha256_crypt
-from data import Articles
-from functools import wraps
 
 app = Flask(__name__)
-Articles = Articles()
 
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
@@ -33,20 +31,28 @@ def about():
 # Articles
 @app.route('/articles')
 def articles():
-    return render_template('articles.html', articles=Articles)
+    # Create cursor
+    cur = mysql.connection.cursor()
+    # Get articles
+    result = cur.execute("SELECT * FROM articles")
+    all_articles = cur.fetchall()
+    if result > 0:
+        return render_template('articles.html', articles=all_articles)
+    # Close connection
+    cur.close()
+    # Message for user
+    msg = 'No articles found.'
+    return render_template('articles.html', msg=msg)
 
 # Single article
 @app.route('/article/<string:id_number>/')
 def article(id_number):
-    return render_template('article.html', id=id_number)
-
-# Register form class
-class RegisterForm(Form):
-    name = StringField('Name', validators=[DataRequired("Please enter your name."), Length(min=1, max=50)])
-    username = StringField('Username', validators=[DataRequired("Please enter your username."), Length(min=4, max=25)])
-    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address.")])
-    password = PasswordField('Password', validators=[DataRequired("Please enter your password."), Length(min=6, message="Passwords must be at least 6 characters long."), EqualTo('confirm', message='Password do not match.')])
-    confirm = PasswordField('Confirm password.', validators=[DataRequired("Please re-enter your password.")])
+    # Create cursor
+    cur = mysql.connection.cursor()
+    # Get article
+    cur.execute("SELECT * FROM articles WHERE id = %s", [id_number])
+    one_article = cur.fetchone()
+    return render_template('article.html', article=one_article)
 
 # User register
 @app.route('/register', methods=['GET', 'POST'])
@@ -87,19 +93,19 @@ def login():
             password = data['password']
             # Compare passwords
             if sha256_crypt.verify(password_candidate, password):
-                # Passed
                 session['logged_in'] = True
                 session['username'] = username
+                # Message for user
                 flash('You are now logged in.', 'success')
                 return redirect(url_for('dashboard'))
-            else:
-                error = 'Wrong password.'
-                return render_template('login.html', error=error)
             # Close connection
             cur.close()
-        else:
-            error = 'Username not found.'
+            # Message for user
+            error = 'Wrong password.'
             return render_template('login.html', error=error)
+        # Message for user
+        error = 'Username not found.'
+        return render_template('login.html', error=error)
     return render_template('login.html')
 
 # Check if user logged in
@@ -108,15 +114,17 @@ def is_logged_in(f):
     def wrap(*args, **kwargs):
         if 'logged_in' in session:
             return f(*args, **kwargs)
-        else:
-            flash('Unauthorized access, please login first.', 'danger')
-            return redirect(url_for('login'))
+        # Message for user
+        flash('Unauthorized access, please login first.', 'danger')
+        return redirect(url_for('login'))
     return wrap
 
 # Logout
 @app.route('/logout')
+@is_logged_in
 def logout():
     session.clear()
+    # Message for user
     flash('You are now logged out.', 'success')
     return redirect(url_for('login'))
 
@@ -124,7 +132,52 @@ def logout():
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-    return render_template('dashboard.html')
+    # Create cursor
+    cur = mysql.connection.cursor()
+    # Get articles
+    result = cur.execute("SELECT * FROM articles")
+    all_articles = cur.fetchall()
+    if result > 0:
+        return render_template('dashboard.html', articles=all_articles)
+    # Close connection
+    cur.close()
+    # Message for user
+    msg = 'No articles found.'
+    return render_template('dashboard.html', msg=msg)
+
+# Add article
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+        # Create cursor
+        cur = mysql.connection.cursor()
+        # Execute query
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", (title, body, session['username']))
+        # Commit to DB
+        mysql.connection.commit()
+        # Close connection
+        cur.close()
+        # Message for user
+        flash('Article created.', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('add_article.html', form=form)
+
+# Register form class
+class RegisterForm(Form):
+    name = StringField('Name', validators=[DataRequired("Please enter your name."), Length(min=1, max=50)])
+    username = StringField('Username', validators=[DataRequired("Please enter your username."), Length(min=4, max=25)])
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your password."), Length(min=6, message="Passwords must be at least 6 characters long."), EqualTo('confirm', message='Password do not match.')])
+    confirm = PasswordField('Confirm password.', validators=[DataRequired("Please re-enter your password.")])
+
+# Article form class
+class ArticleForm(Form):
+    title = StringField('Title', validators=[DataRequired("Please enter article title."), Length(min=1, max=200)])
+    body = TextAreaField('Article', validators=[DataRequired("Please enter content for article."), Length(min=30)])
 
 if __name__ == '__main__':
     app.run(debug=True)
